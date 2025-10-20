7b3dfff1afd7c6ac03fc88a71e3c6b18c3e7e471
theditheredbat <theditheredbat@gmail.com>
2025-10-19 22:37:38 -0700

    Added magit, nord theme, and made other minor changes

--- init.el
+++ init.el
@@ -2,23 +2,21 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(setq inhibit-startup-screen t)
+(setq inhibit-startup-screen  t
+      initial-scratch-message nil)
 
 ;; Cursor Customization
+(global-hl-line-mode)
 (blink-cursor-mode -1)
-(global-hl-line-mode 1)
 
 ;; Frame Format
 (setq frame-title-format "electronic macaronis")
 (add-hook 'emacs-startup-hook 'toggle-frame-maximized)
 
-;; Aesthetics
-(set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 110)
-(load-theme 'modus-operandi-tinted)
-
-;; No Auto-Saves(most of the time)
+;; No Weird File Shenanigans
 (auto-save-mode -1)
-(setq make-backup-files nil)
+(setq make-backup-files nil
+      create-lockfiles  nil)
 
 ;; Move Emacs' Auto-configurations to another file
 (setq custom-file "~/.emacs.d/.emacs.custom.el")
@@ -26,6 +24,13 @@
 
 ;; No funny bell noise(most of the time)
 (setq ring-bell-function 'ignore)
+
+;; Extra Whitespace BEGONE!
+(global-set-key (kbd "C-c s") 'just-one-space)
+
+;; Use UTF-8 Character Encoding
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
 
 ;; Package setup
 (require 'package)
@@ -36,23 +41,34 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Aesthetics
+(set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 110)
+(use-package nord-theme
+  :config
+  (load-theme 'nord t))
+  
 ;; TABS are now SPACES buahahahahhaha
-(setq-default indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil
+              tab-width        4)
 
 ;; c-mode
-(setq-default c-basic-offset 4)
-(global-set-key (kbd "C-c b") 'compile)
+(global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "C-c r") 'recompile)
 
 ;; nasm-mode
 (use-package nasm-mode
   :config
-  (add-to-list 'auto-mode-alist '("\\.asm\\'" . nasm-mode))
-  (setq nasm-basic-offset 4))
+  (add-to-list 'auto-mode-alist '("\\.asm\\'" . nasm-mode)))
 
 ;; org-mode
-(add-hook 'org-mode-hook 'org-indent-mode)
-(setq org-hide-emphasis-markers t)
-(custom-set-faces
- '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.1)))))
+(use-package org
+  :config
+  (add-hook 'org-mode-hook 'org-indent-mode)
+  (custom-set-faces
+   '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+   '(org-level-2 ((t (:inherit outline-2 :height 1.1))))))
+
+;; Git Integration
+(use-package magit)
+
+;; Maybe Add Projectile or something
