a504638c0e3e85aac287e7cce3425dbbe17999af
theditheredbat <theditheredbat@gmail.com>
2025-10-22 22:33:48 -0700

    Removed Org Config

--- init.el
+++ init.el
@@ -4,25 +4,33 @@
 (setq inhibit-startup-screen  t
       initial-scratch-message nil)
 
-(global-hl-line-mode)
-(blink-cursor-mode -1)
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
 
 (setq frame-title-format "electronic macaronis")
 (add-hook 'emacs-startup-hook 'toggle-frame-maximized)
+
+(set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 110)
+
+(blink-cursor-mode -1)
+(global-hl-line-mode)
 
 (auto-save-mode -1)
 (setq make-backup-files nil
       create-lockfiles  nil)
 
+(setq ring-bell-function 'ignore)
+
 (setq custom-file "~/.emacs.d/.emacs.custom.el")
 (load custom-file)
 
-(setq ring-bell-function 'ignore)
+(setq-default indent-tabs-mode nil
+              tab-width        4)
 
 (global-set-key (kbd "C-c s") 'just-one-space)
 
-(set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
+(global-set-key (kbd "C-c c") 'compile)
+(global-set-key (kbd "C-c r") 'recompile)
 
 (require 'package)
 (add-to-list 'package-archives
@@ -32,32 +40,26 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 110)
 (use-package nord-theme
   :config
   (load-theme 'nord t))
-
-(setq-default indent-tabs-mode nil
-              tab-width        4)
-
-(global-set-key (kbd "C-c c") 'compile)
-(global-set-key (kbd "C-c r") 'recompile)
 
 (use-package nasm-mode
   :mode "\\.asm\\'")
 
 (use-package org
+  :defer t
   :config
   (add-hook 'org-mode-hook 'org-indent-mode)
   (custom-set-faces
    '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
    '(org-level-2 ((t (:inherit outline-2 :height 1.1))))))
 
-(use-package magit
-  :defer t)
-
 (use-package vertico
   :init
   (vertico-mode 1)
   :config
   (setq vertico-count 5))
+
+(use-package magit
+  :defer t)
