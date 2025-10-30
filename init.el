950dfb7dc8c30099c4cf59ed52502f0500b8bbde
theditheredbat <theditheredbat@gmail.com>
2025-10-30 01:35:26 -0700

    Swapped Theme and minor changes

--- init.el
+++ init.el
@@ -19,32 +19,50 @@
 (setq make-backup-files nil
       create-lockfiles  nil)
 
+;; Remove Funny Error Noise
 (setq ring-bell-function 'ignore)
 
+;; Move Emacs' auto-configs to another file
 (setq custom-file "~/.emacs.d/.emacs.custom.el")
 (load custom-file)
 
+;; (setq-default display-line-numbers-type 'relative)
+;; (global-display-line-numbers-mode)
+
+;; Tabs are now SPACES buahuahuahauahuahuaha
 (setq-default indent-tabs-mode nil
               tab-width        4)
 
+;; There shall only be ONE!
 (global-set-key (kbd "C-c s") 'just-one-space)
 
+;; c-style things
+(setq c-basic-offset 4) ;; c-default-style
 (global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "C-c r") 'recompile)
+(if (eq system-type 'windows-nt)
+    (setq compile-command "cd .. && build"))
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(setq package-archives
+      '(("melpa-stable" . "https://stable.melpa.org/packages/")
+       ("melpa" . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("melpa-stable" . 10)
+        ("melpa"        . 5)))
+
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package nord-theme
+(use-package kanagawa-themes
   :config
-  (load-theme 'nord t))
+  (load-theme 'kanagawa-dragon t))
 
 (use-package nasm-mode
+  :config
+  (setq nasm-basic-offset 4)
   :mode "\\.asm\\'")
 
 (use-package org
@@ -55,6 +73,7 @@
    '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
    '(org-level-2 ((t (:inherit outline-2 :height 1.1))))))
 
+;; Minibuffer Completions
 (use-package vertico
   :init
   (vertico-mode 1)
