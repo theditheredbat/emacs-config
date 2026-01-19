9c4fc7c7ddc36a295e153228ad645523b7372cf5
theditheredbat <theditheredbat@gmail.com>
2026-01-18 17:18:20 -0800

    a couple changes

--- init.el
+++ init.el
@@ -1,81 +1,89 @@
-;; Disable UI Stuff
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (setq inhibit-startup-screen  t
       initial-scratch-message nil)
 
-;; Cursor Customization
-(global-hl-line-mode)
-(blink-cursor-mode -1)
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8)
 
-;; Frame Format
 (setq frame-title-format "electronic macaronis")
 (add-hook 'emacs-startup-hook 'toggle-frame-maximized)
 
-;; No Weird File Shenanigans
+(set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 120)
+
+(blink-cursor-mode -1)
+(global-hl-line-mode)
+
 (auto-save-mode -1)
 (setq make-backup-files nil
       create-lockfiles  nil)
 
-;; Move Emacs' Auto-configurations to another file
+;; Remove Funny Error Noise, allegedly
+(setq ring-bell-function 'ignore)
+
+;; Move Emacs' auto-configs to another file
 (setq custom-file "~/.emacs.d/.emacs.custom.el")
 (load custom-file)
 
-;; No funny bell noise(most of the time)
-(setq ring-bell-function 'ignore)
+;; Tabs are now SPACES buahuahuahauahuahuaha
+(setq-default indent-tabs-mode nil
+              tab-width        4)
 
-;; Extra Whitespace BEGONE!
+;; There shall only be ONE!
 (global-set-key (kbd "C-c s") 'just-one-space)
 
-;; Use UTF-8 Character Encoding
-(set-language-environment "UTF-8")
-(prefer-coding-system 'utf-8)
+(add-hook 'c-mode-hook (lambda ()
+                         (setq c-basic-offset 4)
+                         (local-set-key (kbd "C-c c") 'compile)
+                         (local-set-key (kbd "C-c r") 'recompile)))
 
-;; Package setup
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(setq package-archives
+      '(("melpa-stable" . "https://stable.melpa.org/packages/")
+       ("melpa" . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("melpa-stable" . 10)
+        ("melpa"        . 5)))
+
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Aesthetics
-(set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 110)
-(use-package nord-theme
-  :config
-  (load-theme 'nord t))
-  
-;; TABS are now SPACES buahahahahhaha
-(setq-default indent-tabs-mode nil
-              tab-width        4)
-
-;; c-mode
-(global-set-key (kbd "C-c c") 'compile)
-(global-set-key (kbd "C-c r") 'recompile)
-
-;; nasm-mode
-(use-package nasm-mode
-  :mode "\\.asm\\'")
-
-;; org-mode
 (use-package org
   :config
-  (add-hook 'org-mode-hook 'org-indent-mode)
+  (add-hook 'org-mode-hook (lambda ()
+                             (org-indent-mode)
+                             (set-fill-column 45)
+                             (auto-fill-mode nil)))
   (custom-set-faces
    '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
    '(org-level-2 ((t (:inherit outline-2 :height 1.1))))))
 
-;; Git Integration
-(use-package magit
-  :defer t)
+(use-package editorconfig
+  :config
+  (editorconfig-mode 1))
 
-;; Completion Framework
+(use-package nasm-mode
+  :config
+  (setq nasm-basic-offset 4)
+  :mode "\\.asm\\'")
+
+;; Look into Afterwriting
+;; Use .fountain for file extension
+(use-package fountain-mode)
+
 (use-package vertico
   :init
-  (vertico-mode 1)
+  (vertico-mode)
   :config
   (setq vertico-count 5))
 
-;; Maybe Add Projectile or something
+(use-package avy
+  :bind
+  (("C-c f" . avy-goto-char-2)))
+
+(use-package lavender-theme
+  :config
+  (load-theme 'lavender t))
